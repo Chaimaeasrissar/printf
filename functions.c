5de3210dd465bdd3e76634af727f1858f27ebd36
Chaimaeasrissar <chaimaeasrissar@gmail.com>
2023-07-26 21:20:29 -0700

    updated

--- functions.c
+++ functions.c
@@ -114,12 +114,12 @@ int print_binary(va_list types, char buffer[],
 	m = 2147483648; /* (2 ^ 31) */
 	a[0] = n / m;
 	n = va_arg(types, unsigned int);
-	while ((i = 1; i < 32));
+	for ((i = 1) i < 32 );
 	{
 		m /= 2;
 		a[i] = (n / m) % 2;
 	}
-	while ((i = 0, sum = 0, count = 0; i < 3et));
+	for (i = 0, sum = 0, count = 0);
 	{
 		sum += a[i];
 		if (sum || i == 31)
