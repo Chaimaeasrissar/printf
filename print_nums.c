7923bcdafafa40c7e8ed3e3a76c72e4f3096feb6
Shaymae1 <Chaymaeelkarcha@gmail.com>
2023-07-28 18:14:00 -0700

    print_nums.c

--- print_nums.c
+++ print_nums.c
@@ -2,74 +2,74 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int print_integer(va_list args, char length_modifier)
+int print_integer(va_list l, char length_modifier)
 {
 	if (length_modifier == 'l')
 	{
-		long int n = va_arg(args, long int);
+		long int n = va_arg(l, long int);
 
 		printf("%ld", n);
 	} else if (length_modifier == 'h')
 	{
-		short int n = va_arg(args, int);
+		short int n = va_arg(l, int);
 
 		printf("%hd", n);
 	} else
 	{
-		int n = va_arg(args, int);
+		int n = va_arg(l, int);
 
 		printf("%d", n);
 	}
 	return (0);
 }
 
-int print_unsigned_integer(va_list args, char length_modifier)
+int print_unsigned_integer(va_list l, char length_modifier)
 {
 	if (length_modifier == 'l')
 	{
-		unsigned long int n = va_arg(args, unsigned long int);
+		unsigned long int n = va_arg(l, unsigned long int);
 
 		printf("%lu", n);
 	} else if (length_modifier == 'h')
 	{
-		unsigned short int n = va_arg(args, unsigned int);
+		unsigned short int n = va_arg(l, unsigned int);
 
 		printf("%hu", n);
 	} else
 	{
-		unsigned int n = va_arg(args, unsigned int);
+		unsigned int n = va_arg(l, unsigned int);
 
 		printf("%u", n);
 	}
 	return (0);
 }
 
-int print_octal(va_list args, char length_modifier)
+int print_octal(va_list l, char length_modifier)
 {
 	if (length_modifier == 'l')
 	{
-		unsigned long int n = va_arg(args, unsigned long int);
+		unsigned long int n = va_arg(l, unsigned long int);
 
 		printf("%lo", n);
 	} else if (length_modifier == 'h')
 	{
-		unsigned short int n = va_arg(args, unsigned int);
+		unsigned short int n = va_arg(l, unsigned int);
 
 		printf("%ho", n);
 	} else
 	{
-		unsigned int n = va_arg(args, unsigned int);
+		unsigned int n = va_arg(l, unsigned int);
 
 		printf("%o", n);
 	}
 	return (0);
 }
 
-int print_hexadecimal(va_list args, char length_modifier, int uppercase)
+int print_hexadecimal(va_list l, char length_modifier, int uppercase)
 {
 	if (length_modifier == 'l')
 	{
-		unsigned long int n = va_arg(args, unsigned long int);
+		unsigned long int n = va_arg(l, unsigned long int);
 
 		if (uppercase)
 		{
@@ -80,7 +80,7 @@ int print_hexadecimal(va_list args, char length_modifier, int uppercase)
 		}
 	} else if (length_modifier == 'h')
 	{
-		unsigned short int n = va_arg(args, unsigned int);
+		unsigned short int n = va_arg(l, unsigned int);
 
 		if (uppercase)
 		{
@@ -91,7 +91,7 @@ int print_hexadecimal(va_list args, char length_modifier, int uppercase)
 		}
 	} else
 	{
-		unsigned int n = va_arg(args, unsigned int);
+		unsigned int n = va_arg(l, unsigned int);
 
 		if (uppercase)
 		{
